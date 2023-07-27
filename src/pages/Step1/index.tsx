ad07fa9106bf62eefbe36429d36cd19f3c71654a
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-07-27 14:55:18 -0300

    colocando o context no step1

--- src/pages/Step1/index.tsx
+++ src/pages/Step1/index.tsx
@@ -2,14 +2,27 @@ import { useNavigate } from "react-router-dom";
 import { Button, Container, Grid } from "@mui/material";
 
 import { BoxForm, Title, Input } from "./styled";
+import { useStep, StepActions } from "../../context/StepContext";
+import { ChangeEvent, useEffect } from "react";
 // import { DateField } from "@mui/x-date-pickers";
 
 export function Step1() {
   const navigate = useNavigate();
+  const { state, dispatch } = useStep();
+
+  useEffect(() => {
+    dispatch({
+      type: StepActions.setCurrentStep,
+      payload: 1,
+    });
+  }, []);
 
   const handleNextStep = () => {
-    navigate("/step2");
-    // console.log("SIIIU");
+    if (state.name !== "") {
+      navigate("/step2");
+    } else {
+      alert("Preencha todos os dados!");
+    }
   };
 
   return (
@@ -18,16 +31,50 @@ export function Step1() {
       <BoxForm onSubmit={handleNextStep}>
         <Grid container spacing={2}>
           <Grid item xs={7}>
-            <Input label="Nome" variant="standard" />
+            <Input
+              label="Nome"
+              variant="standard"
+              value={state.name}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                dispatch({
+                  type: StepActions.setName,
+                  payload: e.target.value,
+                });
+              }}
+            />
           </Grid>
           <Grid item xs={7}>
-            <Input label="Data de Nacimento" variant="standard" />
+            <Input
+              label="Data de Nacimento"
+              variant="standard"
+              value={state.birthday}
+            />
           </Grid>
           <Grid item xs={7}>
-            <Input label="Profissão" variant="standard" />
+            <Input
+              label="Profissão"
+              variant="standard"
+              value={state.profession}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                dispatch({
+                  type: StepActions.setProfession,
+                  payload: e.target.value,
+                });
+              }}
+            />
           </Grid>
           <Grid item xs={7}>
-            <Input label="Email ou telefone" variant="standard" />
+            <Input
+              label="Email ou telefone"
+              variant="standard"
+              value={state.contacts}
+              onChange={(e: ChangeEvent<HTMLInputElement>) => {
+                dispatch({
+                  type: StepActions.setContacts,
+                  payload: e.target.value,
+                });
+              }}
+            />
           </Grid>
         </Grid>
         <Button type="submit">Proximo</Button>
