a3530d503b6df2eebcd989859bf85967a33ec82a
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-08-01 14:24:09 -0300

    Validações dos inputs

--- src/pages/Step1/index.tsx
+++ src/pages/Step1/index.tsx
@@ -1,7 +1,7 @@
 import { useNavigate } from "react-router-dom";
 import { Button, Container, Grid } from "@mui/material";
 
-import { BoxForm, Title, Input } from "./styled";
+import { Title, Input } from "./styled";
 import { useStep, StepActions } from "../../context/StepContext";
 import { ChangeEvent, useEffect } from "react";
 // import { DateField } from "@mui/x-date-pickers";
@@ -18,7 +18,12 @@ export function Step1() {
   }, []);
 
   const handleNextStep = () => {
-    if (state.name !== "") {
+    if (
+      state.name !== "" &&
+      state.birthday !== "" &&
+      state.profession !== "" &&
+      state.contacts !== ""
+    ) {
       navigate("/step2");
     } else {
       alert("Preencha todos os dados!");
@@ -28,57 +33,61 @@ export function Step1() {
   return (
     <Container>
       <Title>Indentficação:</Title>
-      <BoxForm onSubmit={handleNextStep}>
-        <Grid container spacing={2}>
-          <Grid item xs={7}>
-            <Input
-              label="Nome"
-              variant="standard"
-              value={state.name}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                dispatch({
-                  type: StepActions.setName,
-                  payload: e.target.value,
-                });
-              }}
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <Input
-              label="Data de Nacimento"
-              variant="standard"
-              value={state.birthday}
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <Input
-              label="Profissão"
-              variant="standard"
-              value={state.profession}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                dispatch({
-                  type: StepActions.setProfession,
-                  payload: e.target.value,
-                });
-              }}
-            />
-          </Grid>
-          <Grid item xs={7}>
-            <Input
-              label="Email ou telefone"
-              variant="standard"
-              value={state.contacts}
-              onChange={(e: ChangeEvent<HTMLInputElement>) => {
-                dispatch({
-                  type: StepActions.setContacts,
-                  payload: e.target.value,
-                });
-              }}
-            />
-          </Grid>
+      <Grid container spacing={2}>
+        <Grid item xs={7}>
+          <Input
+            label="Nome"
+            variant="standard"
+            value={state.name}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch({
+                type: StepActions.setName,
+                payload: e.target.value,
+              });
+            }}
+          />
         </Grid>
-        <Button type="submit">Proximo</Button>
-      </BoxForm>
+        <Grid item xs={7}>
+          <Input
+            label="Data de Nacimento"
+            variant="standard"
+            value={state.birthday}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch({
+                type: StepActions.setBirthday,
+                payload: e.target.value,
+              });
+            }}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <Input
+            label="Profissão"
+            variant="standard"
+            value={state.profession}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch({
+                type: StepActions.setProfession,
+                payload: e.target.value,
+              });
+            }}
+          />
+        </Grid>
+        <Grid item xs={7}>
+          <Input
+            label="Email ou telefone"
+            variant="standard"
+            value={state.contacts}
+            onChange={(e: ChangeEvent<HTMLInputElement>) => {
+              dispatch({
+                type: StepActions.setContacts,
+                payload: e.target.value,
+              });
+            }}
+          />
+        </Grid>
+      </Grid>
+      <Button onClick={handleNextStep}>Proximo</Button>
     </Container>
   );
 }
--- src/pages/Step1/styled.ts
+++ src/pages/Step1/styled.ts
@@ -3,11 +3,6 @@ import styled from "styled-components";
 
 export const Title = styled.h1``;
 
-export const BoxForm = styled.form`
-  display: flex;
-  flex-direction: column;
-`;
-
 export const Input = styled(TextField)`
   width: 300px;
 `;
