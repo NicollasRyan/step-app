c6ca0fb140d76e6619c551d6136b0249fb563204
Nicollas Ryan <nbarretoduarte@gmail.com>
2023-11-27 16:13:10 -0300

    Validação nos inputs do step1

--- src/pages/Step1/index.tsx
+++ src/pages/Step1/index.tsx
@@ -3,12 +3,21 @@ import { Container, FormLabel, Grid, Button } from "@mui/material";
 
 import { Title, Input, ButtonNext, Input2 } from "./styled";
 import { useStep, StepActions } from "../../context/StepContext";
-import { ChangeEvent, useEffect } from "react";
+import { ChangeEvent, useEffect, useState } from "react";
 // import { DateField } from "@mui/x-date-pickers";
+
+type State = {
+  name: string;
+  birthday: string;
+  profession: string;
+  contacts: string;
+};
 
 export function Step1() {
   const navigate = useNavigate();
   const { state, dispatch } = useStep();
+
+  const [validateFields, setValidateFields] = useState(false);
 
   useEffect(() => {
     dispatch({
@@ -18,6 +27,8 @@ export function Step1() {
   }, []);
 
   const handleNextStep = () => {
+    setValidateFields(true);
+
     if (
       state.name !== "" &&
       state.birthday !== "" &&
@@ -25,10 +36,11 @@ export function Step1() {
       state.contacts !== ""
     ) {
       navigate("/step2");
-    } else {
-      alert("Preencha todos os dados!");
     }
-    navigate("/step2");
+  };
+
+  const isEmpty = (field: keyof State) => {
+    return validateFields && state[field].trim() === "";
   };
 
   return (
@@ -37,9 +49,11 @@ export function Step1() {
       <Grid container spacing={4}>
         <Grid item xs={6}>
           <Input
+            error={isEmpty("name")}
             label="Nome"
             variant="outlined"
             autoComplete="off"
+            helperText={isEmpty("name") ? "Seu nome é obrigatório." : ""}
             value={state.name}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               dispatch({
@@ -51,9 +65,11 @@ export function Step1() {
         </Grid>
         <Grid item xs={6}>
           <Input
+            error={isEmpty("contacts")}
             label="Email ou telefone"
             variant="outlined"
             autoComplete="off"
+            helperText={isEmpty("contacts") ? "email obrigadorio " : ""}
             value={state.contacts}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               dispatch({
@@ -65,9 +81,13 @@ export function Step1() {
         </Grid>
         <Grid item xs={6}>
           <Input
+            error={isEmpty("profession")}
             label="Profissão"
             variant="outlined"
             autoComplete="off"
+            helperText={
+              isEmpty("profession") ? "Qual é sua profissão mesmo?" : ""
+            }
             value={state.profession}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               dispatch({
@@ -80,8 +100,14 @@ export function Step1() {
         <Grid style={{ display: "flex", flexDirection: "column" }} item xs={7}>
           <FormLabel>Data de nacimento</FormLabel>
           <Input2
+            error={isEmpty("birthday")}
             type="date"
             variant="outlined"
+            helperText={
+              isEmpty("birthday")
+                ? "Coloque sua data de nacimento por favor"
+                : ""
+            }
             value={state.birthday}
             onChange={(e: ChangeEvent<HTMLInputElement>) => {
               dispatch({
